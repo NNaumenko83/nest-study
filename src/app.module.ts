590ef241a4a57a4247176c88fab101de1c5cd05d
Mykola Naumenko <108565397+NNaumenko83@users.noreply.github.com>
2025-08-11 13:06:10 +0300

    feat: add TypeORM and configuration support for PostgreSQL
    - Added @nestjs/config and @nestjs/typeorm dependencies to package.json.
    - Configured TypeORM to connect to PostgreSQL using environment variables.
    - Made configuration globally available using ConfigModule.
    - Enabled auto-loading of entities and synchronization in TypeORM configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { MovieModule } from './movie/movie.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [MovieModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Make the configuration globally available
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // Import ConfigModule to access environment variables
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow<string>('POSTGRES_HOST'),
+        port: configService.getOrThrow<number>('POSTGRES_PORT'),
+        username: configService.getOrThrow<string>('POSTGRES_USER'),
+        password: configService.getOrThrow<string>('POSTGRES_PASSWORD'),
+        database: configService.getOrThrow<string>('POSTGRES_DATABASE'),
+        autoLoadEntities: true, // Automatically load entities
+        synchronize: true, // Use with caution in production
+      }),
+      inject: [ConfigService], // Inject ConfigService to access environment variables
+    }), MovieModule],
   controllers: [AppController],
   providers: [AppService],
 })
