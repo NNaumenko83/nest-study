0d1407f561e7617dcd7fc37f8071b2663aa90ba9
Mykola Naumenko <108565397+NNaumenko83@users.noreply.github.com>
2025-08-12 09:41:53 +0300

    feat: create CreateTaskDto with validation rules for title

---
+++ src/tasks/dto/create-task.dto.ts
@@ -0,0 +1,12 @@
+
+import { IsString, MinLength } from "class-validator";
+
+export class CreateTaskDto {
+    @IsString()
+    @MinLength(3)
+
+
+    title: string;
+    description: string;
+    isDone?: boolean;
+}
