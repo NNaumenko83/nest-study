812faf0f9b7135b56369b73620f36a580980ea68
Mykola Naumenko <108565397+NNaumenko83@users.noreply.github.com>
2025-08-12 09:47:15 +0300

    feat: enhance CreateTaskDto with maxLength validation for title and description

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,12 +1,18 @@
 
-import { IsString, MinLength } from "class-validator";
+import { Optional } from "@nestjs/common";
+import { IsString, MaxLength, MinLength } from "class-validator";
 
 export class CreateTaskDto {
     @IsString()
     @MinLength(3)
-
-
+    @MaxLength(50)
     title: string;
+
+    @IsString()
+    @MinLength(5)
+    @MaxLength(200)
     description: string;
+
+    @Optional()
     isDone?: boolean;
 }
