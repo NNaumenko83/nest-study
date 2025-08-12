4e50b60cb8aaee9c2b0072236c67b081c3147882
Mykola Naumenko <108565397+NNaumenko83@users.noreply.github.com>
2025-08-12 09:58:58 +0300

    feat: add TaskEntity and update dependencies for TypeORM integration
    - Added TaskEntity with fields: id, title, description, status, createdAt, and updatedAt.
    - Updated package.json to include @nestjs/typeorm and mysql2 for database support.
    - Added typeorm as a dependency for ORM functionality.

---
+++ src/tasks/entities/task.entity.ts
@@ -0,0 +1,24 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
+
+
+@Entity({ name: 'tasks' })
+export class TaskEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    status: boolean;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
